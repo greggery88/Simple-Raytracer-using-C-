ce46a796c8feb6168269ea46603792515dd0f786
jwilliamson <medbwi@gmail.com>
2024-12-26 11:54:31 -0500

    complete simple diffaction and added

--- color.h
+++ color.h
@@ -10,12 +10,23 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+   if (linear_component > 0.0)
+      return std::sqrt(linear_component);
+   return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color){
 
    // define the x, y, z values as R, G, and B so they can be referenced as such.
    auto r = pixel_color.x();
    auto g = pixel_color.y();
    auto b = pixel_color.z();
+
+   //Apply linear to gamma transform
+   r= linear_to_gamma(r);
+   g= linear_to_gamma(g);
+   b= linear_to_gamma(b);
 
    // convert the RGB [0,1] components of the color vector to bytes range [0,255] so they can be read in the ppm file
    static const interval intensity(0.000,0.999);
