1909e1e01a4b61b9fec145de9c67d9a79b6ef944
jwilliamson <medbwi@gmail.com>
2025-01-01 11:40:11 -0500

    added dielectrics ie. glass

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8,0.8,0));
     auto material_center = make_shared<lambertian>(color(0.1,0.3,0.5));
-    auto material_left = make_shared<metal>(color(0.8,0.8,0.8), 0.1);
+    auto material_bubble = make_shared<dielectric>(1.5);
+    auto material_left = make_shared<dielectric>(1.0/1.5);
     auto material_right = make_shared<metal>(color(0.6,0.4,0.2), 0.5);
 
     world.add(make_shared<sphere>(point3(0.0,-100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0,0.0, -1.2), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.0,0.0, -1.0), 0.5, material_right));
-
+    world.add(make_shared<sphere>(point3(1.0,0.0, -1.0), 0.6, material_right));
+    world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, material_bubble));
 
     camera cam;
 
