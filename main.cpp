5bee6a495cd5244b633adb85644dc0b04774f985
jwilliamson <medbwi@gmail.com>
2025-01-05 18:05:24 -0500

    added cubes

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@
 #include "material.h"
 #include "presets.h"
 #include "bvh.h"
-
+#include "quad.h"
 
 void render_it(camera cam, hittable_list world, char* filename) {
     std::clog << filename <<std::endl;
     world = hittable_list(make_shared<bvh_node>(world));
     cam.render(world, filename);
 }
-
 
 void render_many_balls(camera cam, hittable_list world, char* filename) {
 
@@ -68,6 +67,7 @@ void render_many_balls(camera cam, hittable_list world, char* filename) {
 
     render_it(cam, world, filename);
 }
+
 void render_three_balls_on_green(camera cam, hittable_list world, char* filename) {
 
     auto material_ground = make_shared<lambertian>(color(0.8,0.8,0));
@@ -92,6 +92,7 @@ void render_three_balls_on_green(camera cam, hittable_list world, char* filename
 
     render_it(cam, world, filename);
 }
+
 void render_many_balls_moving(camera cam, hittable_list world, char* filename) {
 
     auto checker = make_shared<checker_texture>(0.32, color(.2,.3,.1), color(.9,.9,.9));
@@ -143,15 +144,16 @@ void render_many_balls_moving(camera cam, hittable_list world, char* filename) {
 
     // set up the camera positioning and focus
     cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
+    cam.lookfrom = point3(-13,8,6);
     cam.lookat   = point3(0,0,0);
     cam.vup      = vec3(0,1,0);
 
-    cam.defocus_angle = 0.6;
+    cam.defocus_angle = 0.3;
     cam.focus_distance    = 10.0;
 
     render_it(cam, world, filename);
 }
+
 void test_if_ray_is_hitting_anything() {
 
         auto material_right = make_shared<metal>(color(0.6,0.4,0.2), 0.5);
@@ -168,6 +170,7 @@ void test_if_ray_is_hitting_anything() {
         if (world.hit(r,interval(0.001, infinity), rec))
                 std::clog << "rays susesfully hitting" << std::endl;
 }
+
 void render_checkered_sphers(camera cam, hittable_list world, char* filename) {
     auto checker = make_shared<checker_texture>(0.32, color(.2,.3,.1), color(.9,.9,.9));
 
@@ -182,6 +185,7 @@ void render_checkered_sphers(camera cam, hittable_list world, char* filename) {
     cam.defocus_angle = 0;
     render_it(cam, world, filename);
 }
+
 void earth(camera cam, hittable_list world, char* filename) {
 
     auto earth_texture = make_shared<image_texture>("Earthmap 1024x512.jpg");
@@ -198,29 +202,90 @@ void earth(camera cam, hittable_list world, char* filename) {
     render_it(cam, world, filename);
 }
 
+void quads(camera cam, hittable_list world, char* filename) {
+
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<Quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<Quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<Quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<Quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<Quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+
+
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    render_it(cam, world, filename);
+}
+
+void cube(camera cam, hittable_list world, char* filename) {
+    double side_length = 10;
+    point3 v1 = point3(0, 0, 0);
+
+    vec3 v2 = vec3(side_length, 0, 0);
+    vec3 v3 = vec3(0,-side_length,0);
+    vec3 v4 = vec3(0,0,side_length);
+
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    world.add(make_shared<Quad>(v1, v2, v3, left_red));
+    world.add(make_shared<Quad>(v1, v3, v4, back_green));
+    world.add(make_shared<Quad>(v1, v4, v2, right_blue));
+
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(-6,4,-5);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    render_it(cam, world, filename);
+}
+
+
 
 int main(int argc, char* argv[]) {
     camera cam;
     hittable_list world;
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 1200;
-    cam.samples_per_pixel = 500;
+    cam.samples_per_pixel = 1000;
     cam.max_depth         = 50;
 
 
-
-char* filename = (char*)"earth.ppm";
+    char* filename = new char[strlen(argv[1])+strlen((char*)".ppm")+1];
+    strcpy(filename, argv[1]);
+    strcat(filename, ".ppm");
+// char* filename = (char*)"earth.ppm";
 
     // std::clog << filename << "\n";
-    switch (2) {
+    switch (6) {
         case 0: test_if_ray_is_hitting_anything(); break;
-
         case 1: render_many_balls(cam, world, filename); break;
-
         case 2: render_many_balls_moving(cam,  world, filename); break;
-
         case 3: render_checkered_sphers(cam, world, filename); break;
         case 4: earth(cam, world, filename); break;
+        case 5: quads(cam, world, filename); break;
+        case 6: cube(cam, world, filename); break;
 
     }
 }
